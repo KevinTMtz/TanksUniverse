a8cf654bc9bf9da6a86027510a3d123f2b820fc8
KevinTMtz <torresmtzkevin@gmail.com>
2021-11-22 14:20:05 -0600

    General info static class

---
+++ Assets/Resources/Scripts/Managers/GeneralInfo.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GeneralInfo
+{
+    public static int numOfPlayers = 1;
+    public static int numOfIAs = 1;
+}
