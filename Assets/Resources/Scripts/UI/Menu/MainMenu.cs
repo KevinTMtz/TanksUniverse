4215325a723805ca7f6485522d4c356294552dbd
KevinTMtz <torresmtzkevin@gmail.com>
2021-11-23 02:03:24 -0600

    Level design with decorations

--- Assets/Resources/Scripts/UI/Menu/MainMenu.cs
+++ Assets/Resources/Scripts/UI/Menu/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayTest()
     {
-        SceneManager.LoadScene("Test");
+        SceneManager.LoadScene("Level1");
     }
 
     public void QuitGame()
