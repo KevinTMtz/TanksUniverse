ef2bed2a5a03f370bc36746bbe9733ac8aa40898
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2021-11-23 00:52:04 -0600

    Turn based game controller (#33)
    * Fixed spawn random tank rotation
    
    * Shuffle players order
    
    * Turn per player
    
    * Tanks array to generic list

--- Assets/Resources/Scripts/Managers/GameManager.cs
+++ Assets/Resources/Scripts/Managers/GameManager.cs
@@ -13,6 +13,19 @@ public class GameManager : MonoBehaviour
     
     private List<GameObject> playerTanks;
     private List<GameObject> computerTanks;
+
+    private int currentPlayer;
+    private GameObject currentPlayerGO;
+    private PlayerController currentPlayerController;
+
+    private bool hasToSetStartState;
+
+    private float timeAbleToShoot;
+    private bool waitingToShoot;
+
+    private bool waitingForBullet;
+
+    private string message;
     
     void Start()
     {
@@ -27,16 +40,76 @@ public class GameManager : MonoBehaviour
 
         computerTanks = new List<GameObject>();
         InstantiateTanks(computerTanks, GeneralInfo.numOfIAs, true);
+
+        Shuffle(playerTanks);
+
+        hasToSetStartState = true;
+        currentPlayer = 0;
+        currentPlayerGO = null;
+        currentPlayerController = null;
     }
 
     void Update()
     {
-        
+        // Wait for tanks to set their state
+        if (hasToSetStartState && Time.frameCount > 1)
+        {
+            for (int i=0; i<playerTanks.Count; i++)
+            {
+                playerTanks[i].transform.Find("PlayerTank").GetComponent<PlayerController>().enabled = false;
+            }
+
+            for (int i=0; i<computerTanks.Count; i++)
+            {
+                computerTanks[i].transform.Find("PlayerTank").GetComponent<PlayerController>().enabled = false;
+            }
+
+            hasToSetStartState = false;
+        }
+
+        // Activate current player in turn
+        if (!hasToSetStartState && currentPlayerGO == null)
+        {
+            playerTanks[currentPlayer].transform.Find("Back Camera").gameObject.SetActive(true);
+
+            currentPlayerGO = playerTanks[currentPlayer].transform.Find("PlayerTank").gameObject;
+            currentPlayerController = currentPlayerGO.GetComponent<PlayerController>();
+            currentPlayerController.enabled = true;
+
+            timeAbleToShoot = Time.time + 3;
+            waitingToShoot = true;
+            waitingForBullet = true;
+        }
+
+        // Wait to be able to shoot
+        if (Time.time > timeAbleToShoot && waitingToShoot)
+        {
+            currentPlayerController.ableToShoot = true;
+            waitingToShoot = false;
+        }
+
+        // When player shoots and bullets crashes deactivate player and go to next one
+        if (
+            currentPlayerGO != null && 
+            !waitingToShoot &&
+            !currentPlayerController.ableToShoot &&
+            !waitingForBullet)
+        {
+            Debug.Log(message);
+
+            playerTanks[currentPlayer].transform.Find("Back Camera").gameObject.SetActive(false);
+            currentPlayerController.enabled = false;
+
+            currentPlayerGO = null;
+            currentPlayerController = null;
+
+            currentPlayer = (currentPlayer + 1) % playerTanks.Count;
+        }
     }
 
-    void InstantiateTanks(List<GameObject> tanksList, int numOfTanks, bool isEnemy)
+    void InstantiateTanks(List<GameObject> tanksList, int numOfElements, bool isEnemy)
     {
-        for (int i=0; i<numOfTanks; i++)
+        for (int i=0; i<numOfElements; i++)
         {
             int randomIndex = Random.Range(0, spawnPoints.Count - 1);
             Vector3 spawnPointsPos = spawnPoints[randomIndex].transform.position;
@@ -54,10 +127,14 @@ public class GameManager : MonoBehaviour
                 )
             );
 
-            tanksList[i].name = isEnemy ? $"PlayerKit {i+1}" : $"PlayerKit {i+1}";
+            tanksList[i].name = isEnemy ? $"Enemy {i+1}" : $"PlayerKit {i+1}";
+
+            tanksList[i].transform.Find("Back Camera").gameObject.SetActive(false);
 
             GameObject playerTank = tanksList[i].transform.Find("PlayerTank").gameObject;
-            playerTank.GetComponent<PlayerController>().TankRotAY = Random.Range(0, 359);
+
+            PlayerController playerController = playerTank.GetComponent<PlayerController>();
+            playerController.TankRotAY = Random.Range(0, 359);
 
             string[] partStrArr = new string[]{"Body", "Tower", "Canon"};
             for (int j=0; j<partStrArr.Length; j++)
@@ -65,6 +142,17 @@ public class GameManager : MonoBehaviour
                 MeshRenderer meshRenderer = playerTank.transform.Find(partStrArr[j]).GetComponent<MeshRenderer>();
                 meshRenderer.materials = new Material[] {materials[isEnemy ? 4 : i]};
             }
+        }
+    }
+
+    void Shuffle(List<GameObject> array)
+    {
+        for (int i = 0; i < array.Count; i++)
+        {
+            int random = Random.Range(0, array.Count);
+            GameObject temp = array[random];
+            array[random] = array[i];
+            array[i] = temp;
         }
     }
 }
--- Assets/Resources/Scripts/Player/PlayerController.cs
+++ Assets/Resources/Scripts/Player/PlayerController.cs
@@ -43,7 +43,7 @@ public class PlayerController : MonoBehaviour
 
     private PlayerTankInfoUI playerTankInfoUI;
 
-    private bool ableToShoot;
+    public bool ableToShoot;
 
     public float TankRotAY
     {
@@ -73,7 +73,6 @@ public class PlayerController : MonoBehaviour
         tankPosDelta = new Vector3(0, 0, 0);
         towerPos = Tower.transform.position;
 
-        tankRotAY = 0;
         towerRotAY = 0;
         canonRotAX = 0;
         lWheelRotAX = 0;
@@ -93,7 +92,7 @@ public class PlayerController : MonoBehaviour
             transform.position.z + tankPosDelta.z
         ));
 
-        ableToShoot = true;
+        ableToShoot = false;
 
         // Constants
         deltaTankMoveMulti = 0.04f;
